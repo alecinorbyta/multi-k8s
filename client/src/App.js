b2a331861a2446b66c267f3faabe33e55a323a77
Alessandro Cinelli <alessandro.cinelli@orbyta.it>
2023-05-12 16:29:44 +0200

    DIO CANE

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            PORCODDIO CANE LURIDO BASTARDO SCHIFOSO
+            PORCODDIO CANE LURIDO BASTARDO SCHIFOSO STRONZO
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
